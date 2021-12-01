13ae550a017c1f8928ed6147a72a1cbc3e39ba4d
Stuart McHattie <stuart.mchattie@gmail.com>
2021-12-01 20:26:05 +0000

    Add day 1 part 2 solution

--- day01/solution.js
+++ day01/solution.js
@@ -1,4 +1,5 @@
 import { readIntList } from '../lib/file-access.js'
+import { sumArray } from '../lib/lib.js'
 
 const rawDepths = readIntList('./day01/input.txt')
 
@@ -8,3 +9,13 @@ for (var i = 1; i < rawDepths.length; i++) {
 }
 
 console.log('Part 1:  ' + increases)
+
+
+increases = 0
+for (var i = 3; i < rawDepths.length; i++) {
+  const prevWindow = sumArray(rawDepths.slice(i - 3, i))
+  const curWindow = sumArray(rawDepths.slice(i - 2, i + 1))
+  increases += curWindow > prevWindow ? 1 : 0
+}
+
+console.log('Part 2:  ' + increases)
