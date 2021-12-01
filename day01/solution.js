4a0b8f2f921d149cf22675135bb779e414dba2ea
Stuart McHattie <stuart.mchattie@gmail.com>
2021-12-01 20:03:55 +0000

    Add day 1 part 1 solution

--- day01/solution.js
+++ day01/solution.js
@@ -1,1 +1,10 @@
-console.log("Hello world!")
+import { readIntList } from '../lib/file-access.js'
+
+const rawDepths = readIntList('./day01/input.txt')
+
+var increases = 0
+for (var i = 1; i < rawDepths.length; i++) {
+  increases += rawDepths[i] > rawDepths[i - 1] ? 1 : 0
+}
+
+console.log('Part 1:  ' + increases)
