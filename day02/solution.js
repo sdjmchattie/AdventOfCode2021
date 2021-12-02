f2dac859b37e0db6fed84a066072ad117eaf73d2
Stuart McHattie <stuart.mchattie@gmail.com>
2021-12-02 08:19:45 +0000

    Update day 2 solution with part 2

--- day02/solution.js
+++ day02/solution.js
@@ -1,22 +1,24 @@
 import { readLines } from '../lib/file-access.js'
 import { productArray } from '../lib/array-func-tools.js'
 
-const input = readLines('./day02/input.txt', (line) => {
+const rawInput = readLines('./day02/input.txt')
+
+const deltas = rawInput.filter((line) => line.length > 0).map((line) => {
   const parts = line.split(' ')
   const direction = parts[0]
-  const distance = parseInt(parts[1])
+  const delta = parseInt(parts[1])
 
   switch (direction) {
     case 'forward':
-      return [distance, 0]
+      return [delta, 0]
     case 'up':
-      return [0, -1 * distance]
+      return [0, -1 * delta]
     case 'down':
-      return [0, distance]
+      return [0, delta]
   }
-}).filter((x) => x !== undefined)
+})
 
-const finalPosition = input.reduce(
+const finalPosition = deltas.reduce(
   (acc, current) => [acc[0] + current[0], acc[1] + current[1]],
   [0, 0]
 )
@@ -25,5 +27,19 @@ console.log('Part 1:')
 console.log('  Final position:  ' + finalPosition)
 console.log('  Product of position:  ' + productArray(finalPosition))
 
+
+const finalState = deltas.reduce(
+  ({ aim, position }, current) => {
+    position[0] += current[0]
+    position[1] += current[0] * aim
+    aim += current[1]
+
+    return { aim, position }
+  },
+  { aim: 0, position: [0, 0] }
+)
+
 console.log()
 console.log('Part 2:  ')
+console.log('  Final position:  ' + finalState.position)
+console.log('  Product of position:  ' + productArray(finalState.position))
