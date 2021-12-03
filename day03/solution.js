2d5331845f446776f0f3759b07169077e0e072a1
Stuart McHattie <sm49@sanger.ac.uk>
2021-12-03 09:56:22 +0000

    Add solution to day 3 part 1

--- day03/solution.js
+++ day03/solution.js
@@ -2,6 +2,16 @@ import { readLines } from '../lib/file-access.js'
 
 const rawInput = readLines('./day03/input.txt')
 
-console.log('Part 1:  ')
+const summedBits = rawInput.reduce((acc, line) => {
+  return acc.map((_, i) => acc[i] + parseInt(line[i]))
+}, Array(rawInput[0].length).fill(0))
+
+const thresh = rawInput.length / 2
+const binaryGammaRate = summedBits.map((bitSum) => bitSum > thresh ? '1' : '0').join('')
+const gammaRate = parseInt(binaryGammaRate, 2)
+const binaryEpsilonRate = summedBits.map((bitSum) => bitSum > thresh ? '0' : '1').join('')
+const epsilonRate = parseInt(binaryEpsilonRate, 2)
+
+console.log('Part 1:  ' + gammaRate * epsilonRate)
 
 console.log('Part 2:  ')
