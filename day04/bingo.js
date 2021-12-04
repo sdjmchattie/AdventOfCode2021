48cd9b5ebb0689f4206a8471815af376567351d1
Stuart McHattie <stuart.mchattie@tessella.com>
2021-12-04 18:19:53 +0000

    Add solution for day 4 part 1

--- day04/bingo.js
+++ day04/bingo.js
@@ -1,5 +1,31 @@
+import { sumArray, transposeArray } from '../lib/array-func-tools.js'
+
 export default class Bingo {
-  constructor(board) {
+  constructor(board, drawnNumbers) {
     this.board = board
+    this.drawnNumbers = drawnNumbers
+  }
+
+  get lines() {
+    if (!this._lines) {
+      this._lines = [...this.board, ...transposeArray(this.board)]
+    }
+    return this._lines
+  }
+
+  get winningDraw() {
+    if (!this._winningDraw) {
+      this._winningDraw = this.lines.reduce((acc, line) => {
+        const winningDrawIndexes = line.map((v) => this.drawnNumbers.indexOf(v))
+        return Math.min(acc, Math.max(...winningDrawIndexes))
+      }, 100)
+    }
+    return this._winningDraw
+  }
+
+  get score() {
+    const allNumbers = this.board.flat()
+    const remainingNumbers = allNumbers.filter((v) => this.drawnNumbers.indexOf(v) > this.winningDraw)
+    return sumArray(remainingNumbers) * this.drawnNumbers[this.winningDraw]
   }
 }
--- day04/solution.js
+++ day04/solution.js
@@ -1,20 +1,22 @@
 import { readLines } from '../lib/file-access.js'
 import Bingo from './bingo.js'
 
-const rawInput = readLines('./day04/input.txt')
+const rawInput = readLines('./day04/input.txt', false)
 const drawnNumbers = rawInput[0].split(',').map((n) => parseInt(n))
 const boards = []
 var board = []
 rawInput.slice(2).forEach((line) => {
   if (line.length === 0) {
-    boards.push(new Bingo(board))
+    boards.push(new Bingo(board, drawnNumbers))
     board = []
     return
   }
 
-  board.push(line.split(/\s+/).map((n) => parseInt(n)))
+  board.push(line.trim().split(/\s+/).map((n) => parseInt(n)))
 })
 
-console.log('Part 1:  ')
+const winningBoard = boards.sort((boardA, boardB) => boardA.winningDraw < boardB.winningDraw ? -1 : 1)[0]
+
+console.log('Part 1:  ' + winningBoard.score)
 
 console.log('Part 2:  ')
