d2787255a3e91249047df6c26e3cad4ec6f50863
Stuart McHattie <stuart.mchattie@tessella.com>
2021-12-04 17:11:11 +0000

    Read and parse input file for day 4

---
+++ day04/bingo.js
@@ -0,0 +1,5 @@
+export default class Bingo {
+  constructor(board) {
+    this.board = board
+  }
+}
--- day04/solution.js
+++ day04/solution.js
@@ -1,6 +1,19 @@
 import { readLines } from '../lib/file-access.js'
+import Bingo from './bingo.js'
 
 const rawInput = readLines('./day04/input.txt')
+const drawnNumbers = rawInput[0].split(',').map((n) => parseInt(n))
+const boards = []
+var board = []
+rawInput.slice(2).forEach((line) => {
+  if (line.length === 0) {
+    boards.push(new Bingo(board))
+    board = []
+    return
+  }
+
+  board.push(line.split(/\s+/).map((n) => parseInt(n)))
+})
 
 console.log('Part 1:  ')
 
