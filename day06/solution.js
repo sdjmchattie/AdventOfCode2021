9a5251ff2e07ae1ee1eee0eeba273a08ea527585
Stuart McHattie <sm49@sanger.ac.uk>
2021-12-06 09:48:12 +0000

    Update solution for day 6 part 1 and solve part 2

--- day06/solution.js
+++ day06/solution.js
@@ -1,14 +1,28 @@
 import { readLines } from '../lib/file-access.js'
+import { countValues, sumArray } from '../lib/array-func-tools.js'
+
+function growOneDay(fishCounts) {
+  const reproducingFish = fishCounts[0] || 0
+  ; [...Array(8).keys()].forEach((i) => fishCounts[i] = fishCounts[i + 1] || 0)
+  fishCounts[6] = fishCounts[6] + reproducingFish
+  fishCounts[8] = reproducingFish
+  return fishCounts
+}
 
 const rawInput = readLines('./day06/input.txt')
 var lanternFish = rawInput[0].split(',').map((x) => parseInt(x))
+const fishCountsPart1 = countValues(lanternFish)
 
 for (var i = 0; i < 80; i++) {
-  const newFish = lanternFish.filter((v) => v === 0).length
-  const nextGeneration = lanternFish.map((f) => f === 0 ? 6 : f - 1)
-  lanternFish = [...nextGeneration, ...Array(newFish).fill(8)]
+  growOneDay(fishCountsPart1)
 }
 
-console.log('Part 1:  ' + lanternFish.length)
+console.log('Part 1:  ' + sumArray(Object.values(fishCountsPart1)))
 
-console.log('Part 2:  ')
+
+const fishCountsPart2 = countValues(lanternFish)
+for (var i = 0; i < 256; i++) {
+  growOneDay(fishCountsPart2)
+}
+
+console.log('Part 2:  ' + sumArray(Object.values(fishCountsPart2)))
