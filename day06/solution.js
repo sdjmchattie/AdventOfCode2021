9f28ec464bd1fe8e0adb9d6a085613e158e42360
Stuart McHattie <sm49@sanger.ac.uk>
2021-12-06 09:25:35 +0000

    Add solution for day 6 part 1

--- day06/solution.js
+++ day06/solution.js
@@ -1,7 +1,14 @@
 import { readLines } from '../lib/file-access.js'
 
 const rawInput = readLines('./day06/input.txt')
+var lanternFish = rawInput[0].split(',').map((x) => parseInt(x))
 
-console.log('Part 1:  ')
+for (var i = 0; i < 80; i++) {
+  const newFish = lanternFish.filter((v) => v === 0).length
+  const nextGeneration = lanternFish.map((f) => f === 0 ? 6 : f - 1)
+  lanternFish = [...nextGeneration, ...Array(newFish).fill(8)]
+}
+
+console.log('Part 1:  ' + lanternFish.length)
 
 console.log('Part 2:  ')
