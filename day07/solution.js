fa456f15701283feee82c6990c37e7bb69b2a02f
Stuart McHattie <sm49@sanger.ac.uk>
2021-12-07 10:04:34 +0000

    Add solution for day 7 part 1
    To be honest, this was a complete guess and it happened to work.  Not sure if it works in all cases.

--- day07/solution.js
+++ day07/solution.js
@@ -1,7 +1,26 @@
 import { readLines } from '../lib/file-access.js'
 
-const rawInput = readLines('./day07/input.txt')
+function median(values){
+  if(values.length ===0) throw new Error("No inputs");
 
-console.log('Part 1:  ')
+  values.sort(function(a,b){
+    return a-b;
+  });
+
+  var half = Math.floor(values.length / 2);
+
+  if (values.length % 2)
+    return values[half];
+
+  return (values[half - 1] + values[half]) / 2.0;
+}
+
+const rawInput = readLines('./day07/input.txt')
+const positions = rawInput[0].split(',').map((x) => parseInt(x))
+const medianPos = median(positions)
+
+const totalFuel = positions.reduce((acc, x) => acc + Math.abs(medianPos - x), 0)
+
+console.log('Part 1:  ' + totalFuel)
 
 console.log('Part 2:  ')
