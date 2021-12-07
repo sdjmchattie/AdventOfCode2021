7614033d9269e88553cb7f9dbea1dc5dfb219058
Stuart McHattie <sm49@sanger.ac.uk>
2021-12-07 10:10:03 +0000

    Tidy up median code

--- day07/solution.js
+++ day07/solution.js
@@ -1,18 +1,10 @@
 import { readLines } from '../lib/file-access.js'
 
 function median(values){
-  if(values.length ===0) throw new Error("No inputs");
-
-  values.sort(function(a,b){
-    return a-b;
-  });
-
-  var half = Math.floor(values.length / 2);
-
-  if (values.length % 2)
-    return values[half];
-
-  return (values[half - 1] + values[half]) / 2.0;
+  if (values.length === 0) { throw new Error("No inputs") }
+  values.sort((a, b) => a - b)
+  const half = Math.floor(values.length / 2)
+  return values.length % 2 ? values[half] : (values[half - 1] + values[half]) / 2.0
 }
 
 const rawInput = readLines('./day07/input.txt')
