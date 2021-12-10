00c45a622c8250ba66099eb84fe5027dc1ca2ec2
Stuart McHattie <stuart.mchattie@tessella.com>
2021-12-10 12:26:39 +0000

    Add day 10 part 1 solution

--- day10/solution.js
+++ day10/solution.js
@@ -1,7 +1,31 @@
 import { readLines } from '../lib/file-access.js'
 
+const braceComplement = { '(': ')', '[': ']', '{': '}', '<': '>' }
+const braceScore = { ')': 3, ']': 57, '}': 1197, '>': 25137 }
+
+function syntaxErrorScore(line) {
+  const expected = []
+  for(var char of line) {
+    switch (char) {
+      case '(':
+      case '[':
+      case '{':
+      case '<':
+        expected.push(braceComplement[char])
+        break
+      case ')':
+      case ']':
+      case '}':
+      case '>':
+        if (expected.pop() !== char) { return braceScore[char] }
+    }
+  }
+
+  return 0
+}
+
 const rawInput = readLines('./day10/input.txt')
 
-console.log('Part 1:  ')
+console.log('Part 1:  ' + rawInput.reduce((acc, line) => acc + syntaxErrorScore(line), 0))
 
 console.log('Part 2:  ')
