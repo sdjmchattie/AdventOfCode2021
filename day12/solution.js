766f1114d4a2c840d5946ab2825062fd50d11532
Stuart McHattie <stuart.mchattie@tessella.com>
2021-12-12 15:12:02 +0000

    Add day 12 part 2 solution

--- day12/solution.js
+++ day12/solution.js
@@ -1,7 +1,12 @@
 import { readLines } from '../lib/file-access.js';
 import _ from 'lodash';
 
-function buildPaths(routes, paths = [['start']]) {
+function twiceVisitedSmallCave(path) {
+  const smallCaves = path.filter((cave) => cave.match(/^[a-z]+$/));
+  return smallCaves.length !== _.uniq(smallCaves).length;
+}
+
+function buildPaths(routes, oneCaveTwice, paths = [['start']]) {
   const pathsToBuild = paths.filter((path) => _.last(path) !== 'end');
   if (pathsToBuild.length === 0) {
     return paths;
@@ -13,11 +18,15 @@ function buildPaths(routes, paths = [['start']]) {
     const nextCaves = routes[_.last(path)].filter((cave) => {
       const bigCave = cave.match(/^[A-Z]+$/) !== null;
       const alreadyVisited = path.includes(cave);
-      return !alreadyVisited || bigCave;
+      return (
+        !alreadyVisited ||
+        (cave !== 'start' && oneCaveTwice && !twiceVisitedSmallCave(path)) ||
+        bigCave
+      );
     });
     return nextCaves.map((cave) => [...path, cave]);
   });
-  return buildPaths(routes, [...filteredPaths, ...extendedPaths]);
+  return buildPaths(routes, oneCaveTwice, [...filteredPaths, ...extendedPaths]);
 }
 
 const rawInput = readLines('./day12/input.txt');
@@ -28,8 +37,10 @@ const routes = rawInput.reduce((acc, routeDesc) => {
   return acc;
 }, {});
 
-const paths = buildPaths(routes);
+const pathsPart1 = buildPaths(routes, false);
 
-console.log('Part 1:  ' + paths.length);
+console.log('Part 1:  ' + pathsPart1.length);
 
-console.log('Part 2:  ');
+const pathsPart2 = buildPaths(routes, true);
+
+console.log('Part 2:  ' + pathsPart2.length);
