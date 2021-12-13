fb36682bb747e9a851bdff7ec10e4eb608c10c63
Stuart McHattie <stuart.mchattie@tessella.com>
2021-12-13 08:47:10 +0000

    Add day 13 part 1 solution

--- day13/solution.js
+++ day13/solution.js
@@ -1,7 +1,24 @@
-import { readLines } from '../lib/file-access.js'
+import { readLines } from '../lib/file-access.js';
+import _ from 'lodash';
 
-const rawInput = readLines('./day13/input.txt')
+function foldAt(coords, axis, fold) {
+  const updatedCoords = coords.map((coord) => {
+    var newCoord = [...coord];
+    if (newCoord[axis] > fold) {
+      newCoord[axis] = 2 * fold - newCoord[axis];
+    }
+    return newCoord;
+  });
+  return _.uniqWith(updatedCoords, _.isEqual);
+}
 
-console.log('Part 1:  ')
+const rawInput = readLines('./day13/input.txt');
+const coords = rawInput
+  .slice(0, 835)
+  .map((c) => c.split(',').map((v) => parseInt(v)));
 
-console.log('Part 2:  ')
+const folded = foldAt(coords, 0, 655);
+
+console.log('Part 1:  ' + folded.length);
+
+console.log('Part 2:  ');
