f8459ecc53797e160ade7ff49524d075a93db687
Stuart McHattie <stuart.mchattie@tessella.com>
2021-12-14 09:01:28 +0000

    Add day 14 part 2 solution

--- day14/solution.js
+++ day14/solution.js
@@ -24,7 +24,6 @@ var chain = startChain;
 for (var i = 0; i < 10; i++) {
   chain = step(chain);
 }
-
 const counts = countValues(chain.split(''));
 
 console.log(
@@ -32,4 +31,46 @@ console.log(
     (maxArray(Object.values(counts)) - minArray(Object.values(counts)))
 );
 
-console.log('Part 2:  ');
+const makeCountingDictWithKeys = (keys) =>
+  keys.reduce((acc, key) => {
+    return { ...acc, [key]: 0 };
+  }, {});
+
+const newStepFunc = (componentMap) =>
+  function (singleCounts, pairCounts) {
+    const newPairCounts = makeCountingDictWithKeys(Object.keys(pairCounts));
+    Object.keys(pairCounts).forEach((key) => {
+      const qty = pairCounts[key];
+      const newComponent = componentMap[key];
+      newPairCounts[key[0] + newComponent] += qty;
+      newPairCounts[newComponent + key[1]] += qty;
+      singleCounts[newComponent] += qty;
+    });
+
+    return [singleCounts, newPairCounts];
+  };
+
+var singleCounts = makeCountingDictWithKeys(
+  _.uniq(Object.values(componentMap))
+);
+var pairCounts = makeCountingDictWithKeys(Object.keys(componentMap));
+
+var chain = startChain;
+for (var i = 0; i < chain.length - 1; i++) {
+  singleCounts[chain[i]] += 1;
+  pairCounts[chain.slice(i, i + 2)] += 1;
+}
+singleCounts[chain[chain.length - 1]] += 1;
+
+const newStep = newStepFunc(componentMap);
+for (var i = 0; i < 40; i++) {
+  [singleCounts, pairCounts] = newStep(singleCounts, pairCounts);
+}
+
+console.log(singleCounts);
+
+console.log(
+  'Part 2:  ' +
+    (maxArray(Object.values(singleCounts)) -
+      minArray(Object.values(singleCounts)))
+);
