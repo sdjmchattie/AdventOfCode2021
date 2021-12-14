aa5dd4069e352c11a089f56ff03df16c71771946
Stuart McHattie <stuart.mchattie@tessella.com>
2021-12-14 08:21:49 +0000

    Add day 14 part 1 solution

--- day14/solution.js
+++ day14/solution.js
@@ -1,7 +1,35 @@
-import { readLines } from '../lib/file-access.js'
+import _ from 'lodash';
+import { countValues, maxArray, minArray } from '../lib/array-func-tools.js';
+import { readLines } from '../lib/file-access.js';
 
-const rawInput = readLines('./day14/input.txt')
+const stepFunc = (componentMap) =>
+  function (chain) {
+    const newComponents = chain.split('').reduce((acc, _, i) => {
+      const key = chain.slice(i, i + 2);
+      return acc + (componentMap[key] || '');
+    }, '');
 
-console.log('Part 1:  ')
+    return _.zip(chain.split(''), newComponents.split('')).flat().join('');
+  };
 
-console.log('Part 2:  ')
+const rawInput = readLines('./day14/input.txt');
+const startChain = rawInput[0];
+const componentMap = rawInput.slice(2).reduce((acc, item) => {
+  const matches = item.match(/(..) -> (.)/);
+  return { ...acc, [matches[1]]: matches[2] };
+}, {});
+const step = stepFunc(componentMap);
+
+var chain = startChain;
+for (var i = 0; i < 10; i++) {
+  chain = step(chain);
+}
+
+const counts = countValues(chain.split(''));
+
+console.log(
+  'Part 1:  ' +
+    (maxArray(Object.values(counts)) - minArray(Object.values(counts)))
+);
+
+console.log('Part 2:  ');
