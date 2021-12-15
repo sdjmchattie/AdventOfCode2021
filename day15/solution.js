80b4a9a179d7f7ae8cf151ae645926d61b502f8d
Stuart McHattie <stuart.mchattie@tessella.com>
2021-12-15 21:00:11 +0000

    Add solution for day 15 part 1

--- day15/solution.js
+++ day15/solution.js
@@ -1,7 +1,64 @@
-import { readLines } from '../lib/file-access.js'
+import _ from 'lodash';
+import { readLines } from '../lib/file-access.js';
 
-const rawInput = readLines('./day15/input.txt')
+const neighboursFunc =
+  (maxX, maxY) =>
+  ([x, y]) =>
+    [
+      [x - 1, y],
+      [x + 1, y],
+      [x, y - 1],
+      [x, y + 1],
+    ].filter(([x, y]) => x >= 0 && x <= maxX && y >= 0 && y <= maxY);
 
-console.log('Part 1:  ')
+function runDijkstra(distanceMap, start, finish) {
+  const visited = [];
+  const unvisited = [];
+  const distance = {};
+  const maxX = distanceMap[0].length - 1;
+  const maxY = distanceMap.length;
+  const neighbours = neighboursFunc(maxX, maxY);
 
-console.log('Part 2:  ')
+  for (var x = 0; x <= maxX; x++) {
+    for (var y = 0; y < maxY; y++) {
+      const point = [x, y].toString();
+      unvisited.push(point);
+      distance[point] = Infinity;
+    }
+  }
+  distance[start] = 0;
+
+  var current = start.toString();
+  while (current !== finish.toString()) {
+    const [x, y] = current.split(',').map((v) => parseInt(v));
+    const unvisitedNeighbours = neighbours([x, y]).filter((neighbour) =>
+      unvisited.includes(neighbour.toString())
+    );
+    for (var neighbour of unvisitedNeighbours) {
+      const newDist =
+        distanceMap[neighbour[1]][neighbour[0]] + distance[current];
+      const tentDist = distance[neighbour];
+      distance[neighbour] = Math.min(newDist, tentDist);
+    }
+
+    visited.push(current);
+    unvisited.splice(unvisited.indexOf(current), 1);
+
+    current = _.minBy(unvisited, (point) => distance[point]);
+  }
+
+  return distance[finish];
+}
+
+const rawInput = readLines('./day15/input.txt');
+const risks = rawInput.map((row) => row.split('').map((v) => parseInt(v)));
+
+const lowestRisk = runDijkstra(
+  risks,
+  [0, 0],
+  [risks[0].length - 1, risks.length - 1]
+);
+
+console.log('Part 1:  ' + lowestRisk);
+
+console.log('Part 2:  ');
