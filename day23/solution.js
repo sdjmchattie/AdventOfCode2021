07acf78a4e2d823e052b6fc1c8d757f9842f44db
Stuart McHattie <stuart.mchattie@tessella.com>
2021-12-23 23:10:36 +0000

    Add day 23 solutions
    I worked it out by hand with chess pieces -- hence no code to solve it

--- day23/solution.js
+++ day23/solution.js
@@ -1,7 +1,11 @@
-import { readLines } from '../lib/file-access.js'
+import { readLines } from '../lib/file-access.js';
 
-const rawInput = readLines('./day23/input.txt')
+const rawInput = readLines('./day23/input.txt');
 
-console.log('Part 1:  ')
+function waveMagicWand(part) {
+  return part === 1 ? 16157 : 43481;
+}
 
-console.log('Part 2:  ')
+console.log('Part 1:  ' + waveMagicWand(1));
+
+console.log('Part 2:  ' + waveMagicWand(2));
